5e78bf2924ccdfee970cbf33cfda84a859895def
Rejin R <rejinraju1421@gmail.com>
2026-08-06 16:59:22 +0530

    Initial Django project setup

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'products',
+    'cart',
+    'orders',
+    'payments',
+    'dashboard',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +80,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'frozenfoods_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Rejin@123.com',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
